a6f2514868713b2ea02020ba5f4a1686702c32fb
shikhar <deepshikhar24@yahoo.com>
2016-10-17 01:24:57 +0530

    removed unnecessary comments and code reformat

--- app/src/main/java/com/example/android/delhi06/MapsActivity.java
+++ app/src/main/java/com/example/android/delhi06/MapsActivity.java
@@ -14,7 +14,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.ms.square.android.expandabletextview.ExpandableTextView;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {//extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -26,25 +26,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // sample code snippet to set the text content on the ExpandableTextView
         ExpandableTextView expTv1 = (ExpandableTextView) findViewById(R.id.expand_text_view);
 
-// IMPORTANT - call setText on the ExpandableTextView to set the text content to display
+        // IMPORTANT - call setText on the ExpandableTextView to set the text content to display
         expTv1.setText(getString(R.string.dummy_text1));
 
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         SliderLayout sliderShow = (SliderLayout) findViewById(R.id.slider);
-        DefaultSliderView textSliderView = new DefaultSliderView(this);
-        textSliderView
-                .image(R.drawable.pic1).setScaleType(BaseSliderView.ScaleType.FitCenterCrop);
 
+        DefaultSliderView textSliderView = new DefaultSliderView(this);
+        textSliderView.image(R.drawable.pic1).setScaleType(BaseSliderView.ScaleType.FitCenterCrop);
         sliderShow.addSlider(textSliderView);
 
         DefaultSliderView textSliderView2 = new DefaultSliderView(this);
-        textSliderView2
-                .image(R.drawable.pic2);
-                //.setScaleType(BaseSliderView.ScaleType.Fit);
+        textSliderView2.image(R.drawable.pic2);
         sliderShow.addSlider(textSliderView2);
     }
 
@@ -59,48 +55,3 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.moveCamera(CameraUpdateFactory.newLatLng(redFort));
     }
 }
-
-
-
-
-
- /*       SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-}*/
-/*import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.ms.square.android.expandabletextview.ExpandableTextView;
-
-public class MapsActivity extends Activity {
-    TextView descText;
-    ImageButton show, hide;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-
-        // sample code snippet to set the text content on the ExpandableTextView
-        ExpandableTextView expTv1 = (ExpandableTextView) findViewById(R.id.expand_text_view);
-
-// IMPORTANT - call setText on the ExpandableTextView to set the text content to display
-        expTv1.setText(getString(R.string.dummy_text1));
-
-    }
-}*/
