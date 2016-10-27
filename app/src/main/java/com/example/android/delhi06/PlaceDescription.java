771692d4dd4935f2685353ab42c6ee182ea55763
shikhar <deepshikhar24@yahoo.com>
2016-10-27 08:42:40 +0530

    implemented 3rd activity-PlaceDescription.java

--- app/src/main/java/com/example/android/delhi06/PlaceDescription.java
+++ app/src/main/java/com/example/android/delhi06/PlaceDescription.java
@@ -6,7 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class PlaceDescription extends AppCompatActivity {       //see all colors of its layout...sai hai na especially white shades...see 2nd activity ka colors(note it on paper) and incorporate same colors in 3rd activity//
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+
+public class PlaceDescription extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +44,38 @@ public class PlaceDescription extends AppCompatActivity {       //see all colors
                 finish();
             }
         });
+
+        // sample code snippet to set the text content on the ExpandableTextView
+        ExpandableTextView expTv1 = (ExpandableTextView) findViewById(R.id.expand_text_view);
+
+        // IMPORTANT - call setText on the ExpandableTextView to set the text content to display
+        expTv1.setText(getString(R.string.dummy_text1));
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        SliderLayout sliderShow = (SliderLayout) findViewById(R.id.slider);
+
+        DefaultSliderView textSliderView = new DefaultSliderView(this);
+        textSliderView.image(R.drawable.red_fort_1).setScaleType(BaseSliderView.ScaleType.FitCenterCrop);
+        sliderShow.addSlider(textSliderView);
+        sliderShow.setDuration(1500);
+
+        DefaultSliderView textSliderView2 = new DefaultSliderView(this);
+        textSliderView2.image(R.drawable.bangla_sahib_2).setScaleType(BaseSliderView.ScaleType.FitCenterCrop);;
+        sliderShow.addSlider(textSliderView2);
+        sliderShow.setDuration(1500);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+        String loc = "Red Fort";
+
+        // Add a marker at location and move the camera.
+        LatLng redFort = new LatLng(28.656159, 77.241024);
+        mMap.addMarker(new MarkerOptions().position(redFort).title(loc));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(redFort));
     }
 }
